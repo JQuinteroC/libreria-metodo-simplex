740626fa3c14d5d3ab5c7793f931f9a7808c08c6
JQuinteroC <jlqc1772@gmail.com>
2019-04-09 22:21:40 -0500

    Corrección de errores

--- lib_metodo_simplex/src/simplex/Gauss.java
+++ lib_metodo_simplex/src/simplex/Gauss.java
@@ -8,27 +8,27 @@ import java.util.ArrayList;
  */
 public class Gauss {
 
-    void gauss(ArrayList<Double[]> matriz, int numVariables, int numEcuacione) {
+    public void gauss(ArrayList<Double[]> matriz, int numVariables, int numEcuacione) {
         int[] posPivote;
 
-        for (int i = 1; i <= numVariables; i++) {
+        
             posPivote = hallarPivote(matriz, numVariables, numEcuacione);
-            productoPivote(matriz, posPivote, 1/matriz.get(posPivote[0])[posPivote[1]]);
+            double multiplicador = Math.pow(matriz.get(posPivote[0])[posPivote[1]] , -1);
+            productoPivote(matriz, posPivote,multiplicador );
             for (int j = 0; j < numEcuacione + 1; j++) {
-                if (matriz.get(j)[posPivote[1]] != 0) {
-                    double factor = matriz.get(j)[posPivote[1]];
-                    double[] producto = productoFila(matriz, posPivote, 1 / factor);
+                if (matriz.get(j)[posPivote[1]] != 0 && posPivote[0] != j) {
+                    double factor = matriz.get(j)[posPivote[1]] *-1;
+                    double[] producto = productoFila(matriz, posPivote, factor);
                     sumarRenglonesG(matriz, j, producto);
                 }
             }
 
-        }
     }
 
     void sumarRenglonesG(ArrayList<Double[]> sumando, int renglonSuma, double[] sumandoPivote) {
-        Double h[] = null;
+        Double h[] = new Double[sumando.get(0).length];
         for (int i = 0; i < sumando.get(0).length; i++) {
-            h[renglonSuma] = sumando.get(renglonSuma)[i] + sumandoPivote[i];
+            h[i] = sumando.get(renglonSuma)[i] + sumandoPivote[i];
         }
         sumando.set(renglonSuma, h);
     }
@@ -49,7 +49,7 @@ public class Gauss {
                 numeros.add(matriz.get(0)[i]);
             }
         }
-        int col = menorPosicion(numeros);
+        int col = menorPosicion(numeros)+1;
         numeros.clear();
 
         // Encontrar la fila
@@ -57,27 +57,28 @@ public class Gauss {
             double cociente = matriz.get(i)[(numVariables * 2) + 1] / matriz.get(i)[col];
             numeros.add(cociente);
         }
-        int fil = menorPosicion(numeros);
+        int fil = menorPosicion(numeros)+1;
 
         // Resultado
-        int res[] = null;
+        int res[] = new int[2];
         res[0] = fil;
         res[1] = col;
         return res;
     }
 
     /**
-     * Multiplica una final pivote por un número especifico
+     * Multiplica una fila pivote por un número especifico
      *
      * @param matriz
      * @param posPivote
      * @param multiplicador
      */
     void productoPivote(ArrayList<Double[]> matriz, int[] posPivote, double multiplicador) {
-        for (double producto : matriz.get(posPivote[0])) {
-            producto = producto * multiplicador;
-
+        Double[] producto = new Double[matriz.get(posPivote[0]).length];
+        for (int i = 0; i < matriz.get(posPivote[0]).length; i++) {
+            producto[i] = multiplicador * matriz.get(posPivote[0])[i];
         }
+        matriz.set(posPivote[0], producto);
     }
 
     /**
@@ -89,8 +90,8 @@ public class Gauss {
      * @return
      */
     static double[] productoFila(ArrayList<Double[]> matriz, int[] posicion, double multiplicador) {
-        double[] productos = null;
-        for (int i = 0; i < matriz.size(); i++) {
+        double[] productos = new double [matriz.get(posicion[0]).length];
+        for (int i = 0; i < matriz.get(posicion[0]).length; i++) {
             productos[i] = multiplicador * matriz.get(posicion[0])[i];
         }
         return productos;
